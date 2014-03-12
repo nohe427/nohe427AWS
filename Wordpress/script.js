70ae78ea11600a0c9ad1f21dc05d6ec0618203ca
Alexander Nohe <nohe427@users.noreply.github.com>
2014-03-11 23:22:31 -0700

    Update script.js

--- Wordpress/script.js
+++ Wordpress/script.js
@@ -7,3 +7,12 @@ function initialize() {
             mapOptions);
       }
       google.maps.event.addDomListener(window, 'load', initialize);
+      
+      var weatherLayer = new google.maps.weather.WeatherLayer({
+    temperatureUnits: google.maps.weather.TemperatureUnit.FAHRENHEIT
+  });
+  weatherLayer.setMap(map);
+
+  var cloudLayer = new google.maps.weather.CloudLayer();
+  cloudLayer.setMap(map);
+}
