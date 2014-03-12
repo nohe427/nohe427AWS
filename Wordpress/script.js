ab1bc8f3c4e80b3bd68b70e8a2147a9410840bfa
Alexander Nohe <alex.nohe427@gmail.com>
2014-03-11 23:27:24 -0700

    ..

--- Wordpress/script.js
+++ Wordpress/script.js
@@ -1,4 +1,5 @@
 function initialize() {
+<<<<<<< HEAD
   var mapOptions = {
     zoom: 6,
     center: new google.maps.LatLng(49.265984,-123.127491)
@@ -8,6 +9,18 @@ function initialize() {
       mapOptions);
 
   var weatherLayer = new google.maps.weather.WeatherLayer({
+=======
+        var mapOptions = {
+          center: new google.maps.LatLng(38.3453411,-75.6038331),
+          zoom: 16
+        };
+        var map = new google.maps.Map(document.getElementById("map-canvas"),
+            mapOptions);
+      }
+      google.maps.event.addDomListener(window, 'load', initialize);
+      
+      var weatherLayer = new google.maps.weather.WeatherLayer({
+>>>>>>> parent of af8e44e... Update script.js
     temperatureUnits: google.maps.weather.TemperatureUnit.FAHRENHEIT
   });
   weatherLayer.setMap(map);
@@ -15,5 +28,8 @@ function initialize() {
   var cloudLayer = new google.maps.weather.CloudLayer();
   cloudLayer.setMap(map);
 }
+<<<<<<< HEAD
 
 google.maps.event.addDomListener(window, 'load', initialize);
+=======
+>>>>>>> parent of af8e44e... Update script.js
